7fee1bb4a8a7e4d8958115d6ff9d3f9d50b4677f
Duraid Maihoub <duraidmaihoub@gmail.com>
2025-06-17 05:13:33 -0700

    change tty settings to raw and restore them before `xd_readline()` returns

--- src/xd_readline.c
+++ src/xd_readline.c
@@ -16,6 +16,9 @@
 #include "xd_readline.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
 
 // ========================
 // Macros and Constants
@@ -29,9 +32,17 @@
 // Function Declarations
 // ========================
 
+static void xd_tty_raw();
+static void xd_tty_restore();
+
 // ========================
 // Variables
 // ========================
+
+/**
+ * @brief Store the tty attributes when `xd_readline()` was called.
+ */
+static struct termios xd_original_tty_attributes;
 
 // ========================
 // Public Variables
@@ -41,10 +52,48 @@
 // Function Definitions
 // ========================
 
+/**
+ * @brief Changes the terminal input settings to raw.
+ */
+static void xd_tty_raw() {
+  // store original tty attributes
+  if (tcgetattr(STDIN_FILENO, &xd_original_tty_attributes) == -1) {
+    fprintf(stderr, "xd_readline: failed to get tty attributes\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // set tty input to raw
+  struct termios xd_getline_tty_attributes;
+  if (tcgetattr(STDIN_FILENO, &xd_getline_tty_attributes) == -1) {
+    fprintf(stderr, "xd_readline: failed to get tty attributes\n");
+    exit(EXIT_FAILURE);
+  }
+  xd_getline_tty_attributes.c_lflag &= ~(ICANON | ECHO);
+  xd_getline_tty_attributes.c_cc[VTIME] = 0;
+  xd_getline_tty_attributes.c_cc[VMIN] = 1;
+  if (tcsetattr(STDIN_FILENO, TCSANOW, &xd_getline_tty_attributes) == -1) {
+    fprintf(stderr, "xd_readline: failed to set tty attributes\n");
+    exit(EXIT_FAILURE);
+  }
+}  // xd_tty_raw()
+
+/**
+ * @brief Restore original terminal settings.
+ */
+static void xd_tty_restore() {
+  if (tcsetattr(STDIN_FILENO, TCSANOW, &xd_original_tty_attributes) == -1) {
+    fprintf(stderr, "xd_readline: failed to reset tty attributes\n");
+    exit(EXIT_FAILURE);
+  }
+}  // xd_tty_restore()
+
 // ========================
 // Public Functions
 // ========================
 
 char *xd_readline() {
+  xd_tty_raw();
+  // READ
+  xd_tty_restore();
   return NULL;
 }  // xd_readline()
